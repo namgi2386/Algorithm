9c9cf4cbcdf84dd4de846f29341db5edba67bfa0
임남기 <143250820+namgi2386@users.noreply.github.com>
2024-12-04 01:43:03 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import 	java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner scan = new Scanner(System.in);
+		a =scan.nextInt(); // 공백기준 하나씩 가져오기
+		b = scan.nextInt();
+		System.out.println(a+b);
+//		System.out.println((double)a/b);
+		
+		
+		
+	}
+}
