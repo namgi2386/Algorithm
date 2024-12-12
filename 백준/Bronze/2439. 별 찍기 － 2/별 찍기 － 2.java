76ee54e504aae7b052c7d70b7254c4ad9c1abe3e
임남기 <143250820+namgi2386@users.noreply.github.com>
2024-12-12 16:58:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 116 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new FileReader("src/input/z1.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        solution(br,bw);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static void solution(BufferedReader br , BufferedWriter bw )throws IOException{
+        // 코드작성
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = N-1-i; j >0; j--) {
+                bw.write(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        // 코드 끝
+    }
+
+}
