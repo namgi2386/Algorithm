e38c92676c405330f9af55a69e62d9648cf4fe99
임남기 <143250820+namgi2386@users.noreply.github.com>
2025-06-04 11:30:53 +0900

    [level 0] Title: 문자열 출력하기, Time: 45.74 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -1,14 +1,4 @@
-const readlinetemp = require('readline');
-const rltemp = readlinetemp.createInterface({
+require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let inputtemp = [];
-
-rltemp.on('line', function (linetemp) {
-    inputtemp = [linetemp];
-}).on('close',function(){
-    const str = inputtemp[0];
-    console.log(str);
-});
+}).on('line', console.log)
