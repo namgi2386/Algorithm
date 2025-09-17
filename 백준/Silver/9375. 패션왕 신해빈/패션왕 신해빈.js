f97b50124820e2de8cd073c8eb0815f4588c03b1
임남기 <143250820+namgi2386@users.noreply.github.com>
2025-09-18 01:14:37 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 96 ms, Memory: 9860 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
@@ -3,17 +3,17 @@ const inputValue = require("fs").readFileSync(path).toString().trim();
 
 let [TC, ...arr] = inputValue.split("\n").map((c) => c.trim());
 let idx = 0;
-
 for (let tc = 0; tc < TC; tc++) {
   let map = new Map();
   let N = Number(arr[idx]);
+
 
   for (let i = idx + 1; i < idx + N + 1; i++) {
     let type = arr[i].split(" ")[1];
     map.set(type, (map.get(type) || 0) + 1);
   }
   let numArr = [...map.values()].map((c) => c + 1);
-  let answer = numArr.reduce((a, b) => a * b, 1) - 1;
+  let answer = numArr.reduce((a, b) => a * b,1) - 1;
   console.log(answer);
 
   idx += N + 1;
