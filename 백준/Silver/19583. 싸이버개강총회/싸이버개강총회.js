c6b091dcd6fdf9127543cccb8e5cc9ba0d33ad87
임남기 <143250820+namgi2386@users.noreply.github.com>
2025-09-18 11:44:22 +0900

    [Silver II] Title: 싸이버개강총회, Time: 440 ms, Memory: 75904 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.js
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.js
@@ -10,7 +10,7 @@ function realTime(time) {
   return h * 60 + m;
 }
 
-const [T1, T2, T3] = initNums.map(realTime); 
+const [T1, T2, T3] = initNums.map(realTime);
 // 06:00 12:00 18:00 => 360 720 1080
 
 let set = new Set();
@@ -19,9 +19,9 @@ let answerSet = new Set();
 for (let i = 0; i < arr.length; i++) {
   let [time, name] = arr[i];
   let min = realTime(time);
-  if (min ===0 || (min > 0 && min <= T1)) {
+  if (min <= T1) {
     set.add(name);
-  } else if (min === T2 || min === T3 || (min > T2 && min < T3)) {
+  } else if (min >= T2 && min <= T3) {
     if (set.has(name)) answerSet.add(name);
   }
 }
