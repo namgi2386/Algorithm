a00e76d8699aa748aff4d5123830a5b623a1c57f
임남기 <143250820+namgi2386@users.noreply.github.com>
2025-10-27 18:31:28 +0900

    [Platinum II] Title: 문자열의 주기 예측, Time: 228 ms, Memory: 38600 KB -BaekjoonHub

---
+++ 백준/Platinum/1787. 문자열의 주기 예측/문자열의 주기 예측.js
@@ -0,0 +1,32 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const inputValue = require("fs").readFileSync(path).toString().trim();
+
+let [N, arr] = inputValue.split("\n").map((c) => c.trim());
+N = Number(N);
+
+let failArr = new Array(N).fill(0);
+let jIdx = 0; // 기본 fail pointer
+for (let i = 1; i < arr.length; i++) {
+  while (arr[i] !== arr[jIdx] && jIdx > 0) {
+    jIdx = failArr[jIdx - 1];
+  }
+  if (arr[i] === arr[jIdx]) {
+    failArr[i] = ++jIdx;
+  }
+}
+
+const maxPront = Array(N).fill(0);
+let answer = 0;
+
+for (let i = 1; i < N; i++) {
+  // 일치하긴함
+  if (failArr[i] > 0) {
+    const prev = failArr[i] - 1;
+    maxPront[i] = failArr[prev] > 0 ? maxPront[prev] : failArr[i];
+  }
+  if (maxPront[i] === 0) continue;
+  const back = i + 1 - maxPront[i];
+  if (back >= maxPront[i]) answer += back;
+}
+
+console.log(answer);
