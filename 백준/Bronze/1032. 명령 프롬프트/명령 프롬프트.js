c980ce07bf42cede53b6ddc88981183dd560740a
임남기 <143250820+namgi2386@users.noreply.github.com>
2025-11-09 15:29:42 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.js
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.js
@@ -3,11 +3,11 @@ const inputValue = require("fs").readFileSync(path).toString().trim();
 
 const lines = inputValue.split(/\r?\n/);
 const N = Number(lines[0]);
-const files = lines.slice(1, N + 1).map(s => s.split(""));
+const arr = lines.slice(1, N + 1).map(s => s.split(""));
 
-let answer = files[0];
+let answer = arr[0];
 for (let i = 1; i < N; i++) {
-  files[i].forEach((c, idx) => {
+  arr[i].forEach((c, idx) => {
     if (answer[idx] !== c) {
       answer[idx] = "?";
     }
