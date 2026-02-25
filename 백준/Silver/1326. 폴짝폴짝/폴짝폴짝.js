5fdc7924864fb81fbfc46b15f72c6b3c922aa8da
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-02-25 21:34:09 +0900

    [Silver II] Title: 폴짝폴짝, Time: 192 ms, Memory: 20448 KB -BaekjoonHub

--- 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.js
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.js
@@ -6,27 +6,45 @@ let [N, arr, initN] = inputValue
   .map((c) => c.trim().split(" ").map(Number));
 N = Number(N);
 const [s, e] = initN.map((c) => c - 1);
-const dp = new Array(N).fill(Infinity);
+const dp = new Array(N).fill(N + 1);
 dp[s] = 0;
-const stack = [[s, 0]];
 
-while (stack.length > 0) {
-  const [node, cnt] = stack.shift();
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.index = 0;
+    this.peek = 0;
+  }
+  push(val) {
+    this.queue.push(val);
+    this.index += 1;
+  }
+  shift() {
+    if (this.index === this.peek) return null;
+    return this.queue[this.peek++];
+  }
+  size() {
+    return this.index - this.peek;
+  }
+}
+const queue = new Queue();
+queue.push([s, 0]);
+
+while (queue.size() > 0) {
+  const [node, cnt] = queue.shift();
 
   let jump = arr[node];
   for (let i = 1; node + jump * i < N; i++) {
     const next = node + jump * i;
     if (dp[next] < cnt + 1) continue;
     dp[next] = cnt + 1;
-    // console.log("a", node, jump, next, cnt + 1);
-    stack.push([next, cnt + 1]);
+    queue.push([next, cnt + 1]);
   }
   for (let i = 1; node - jump * i >= 0; i++) {
     const next = node - jump * i;
     if (dp[next] < cnt + 1) continue;
     dp[next] = cnt + 1;
-    // console.log("b", node, jump, next, cnt + 1);
-    stack.push([next, cnt + 1]);
+    queue.push([next, cnt + 1]);
   }
 }
-console.log(dp[e] !== Infinity ? dp[e] : -1);
+console.log(dp[e] !== N + 1 ? dp[e] : -1);
