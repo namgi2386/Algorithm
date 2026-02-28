79218fdd31a1a5b65a4ed2e4659f27b42bec8114
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-02-28 17:30:48 +0900

    [Silver II] Title: 퍼거슨과 사과, Time: 84 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/2942. 퍼거슨과 사과/퍼거슨과 사과.js
+++ 백준/Silver/2942. 퍼거슨과 사과/퍼거슨과 사과.js
@@ -1,27 +1,30 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const inputValue = require("fs").readFileSync(path).toString().trim();
 
-let [N, M] = inputValue.split(" ").map(BigInt);
-// 최대공약수
+let [N, M] = inputValue.split(" ").map(Number);
 
 function gcd(a, b) {
-  while(b !== 0n) {
-    var t = b;
+  while (b !== 0) {
+    const t = b;
     b = a % b;
     a = t;
   }
   return a;
 }
+
 const max = gcd(N, M);
 const dn = N / max;
 const dm = M / max;
+let answer = "";
 
-for (let i = 1n; i * i <= max; i++) {
-  if (max % i === 0n) {
+for (let i = 1; i * i <= max; i++) {
+  if (max % i === 0) {
     const j = max / i;
-    console.log(`${i} ${j * dn} ${j * dm}`);
+    answer += `${i} ${j * dn} ${j * dm}\n`;
     if (i !== j) {
-      console.log(`${j} ${i * dn} ${i * dm}`);
+      answer += `${j} ${i * dn} ${i * dm}\n`;
     }
   }
 }
+
+console.log(answer.trim());
