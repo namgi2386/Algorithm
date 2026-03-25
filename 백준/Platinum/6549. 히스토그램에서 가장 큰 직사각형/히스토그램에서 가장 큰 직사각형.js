9c91af42bca462d2081761c897a94252a16b38fd
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-03-25 19:21:27 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 300 ms, Memory: 51004 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.js
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.js
@@ -4,26 +4,27 @@ const inputValue = require("fs")
   .toString()
   .trim()
   .split("\n")
-  .map((c) => c.trim().split(" ").map(BigInt));
+  .map((c) => c.trim().split(" ").map(Number));
 
 for (const arr of inputValue) {
+  arr.push(0);
   const N = arr[0];
-  if (N === 0n) continue;
-  let answer = 0n;
+  if (N === 0) continue;
+  let answer = 0;
   let stack = [];
-  for (let i = 1; i <= N; i++) {
+  for (let i = 1; i <= N + 1; i++) {
     const node = arr[i];
     if (stack.length === 0) {
-      stack.push([node, 1n]);
+      stack.push([node, 1]);
       continue;
     }
     if (stack[stack.length - 1][0] < node) {
-      stack.push([node, 1n]);
+      stack.push([node, 1]);
     } else if (stack[stack.length - 1][0] === node) {
-      stack[stack.length - 1][1] += 1n;
+      stack[stack.length - 1][1]++;
     } else {
       // 스택 마지막 놈이 더 높음
-      let totalCnt = 0n;
+      let totalCnt = 0;
       while (stack.length > 0) {
         const [peek, cnt] = stack.pop();
         if (peek <= node) {
@@ -36,19 +37,9 @@ for (const arr of inputValue) {
         }
         totalCnt += cnt;
       }
-      stack.push([node, totalCnt + 1n]);
+      stack.push([node, ++totalCnt]);
     }
     // console.log(i, stack, answer);
-  }
-  // 1 2 4 5 6 7 6 6 2
-
-  let totalCnt = 0n;
-  for (let i = stack.length - 1; i >= 0; i--) {
-    const temp = stack[i][0] * (totalCnt + stack[i][1]);
-    if (answer < temp) {
-      answer = temp;
-    }
-    totalCnt += stack[i][1];
   }
 
   console.log(answer.toString());
