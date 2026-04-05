e432c730bd22ef49e88b5d5f14433b4e767c76f1
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-04-05 12:36:28 +0900

    [Silver III] Title: N과 M (6), Time: 92 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.js
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -1,24 +1,27 @@
-const path = (process.platform === "linux" ? "/dev/stdin" : "input.txt");
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const inputValue = require("fs").readFileSync(path).toString().trim();
 
-const lines = inputValue.split('\n');
-const [N, M] = lines[0].split(' ').map(Number);
-const numbers = lines[1].split(' ').map(Number).sort((a, b) => a - b);
-const result = [];
+let [initN, arr] = inputValue
+  .split("\n")
+  .map((c) => c.trim().split(" ").map(Number));
+arr.sort((a, b) => a - b);
+const [N, M] = initN;
+
+const stack = [];
 const answer = [];
 
-function dfs(start, depth) {
-    if (depth === M) {
-        answer.push(result.join(' '));
-        return;
-    }
-    
-    for (let i = start; i < N; i++) {
-        result.push(numbers[i]);
-        dfs(i + 1, depth + 1);
-        result.pop();
-    }
+function dfs(visited, cnt, prev) {
+  if (cnt === M) {
+    answer.push(stack.join(" "));
+    return;
+  }
+  for (let i = prev; i <= N; i++) {
+    const node = 1 << i;
+    if (visited & node) continue;
+    stack.push(arr[i - 1]);
+    dfs(visited | node, cnt + 1, i);
+    stack.pop();
+  }
 }
-
-dfs(0, 0);
-console.log(answer.join('\n'));
+dfs(0, 0, 1);
+console.log(answer.join("\n"));
