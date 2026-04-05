0f5b9cf8522188e5986387d38c567e42716e588f
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-04-05 12:28:54 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.js
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -1,19 +1,20 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const inputValue = require("fs").readFileSync(path).toString().trim();
-const [N, M] = inputValue.split(" ").map(Number);
 
+const [N, M] = inputValue.split(" ").map(Number);
 const stack = [];
 const answer = [];
-function dfs(level, start) {
-  if (level === M) {
+
+function dfs(prev, cnt) {
+  if (cnt === M) {
     answer.push(stack.join(" "));
     return;
   }
-  for (let i = start; i < N + 1; i++) {
+  for (let i = prev; i <= N; i++) {
     stack.push(i);
-    dfs(level + 1, i);
+    dfs(i, cnt + 1);
     stack.pop();
   }
 }
-dfs(0, 1);
+dfs(1, 0);
 console.log(answer.join("\n"));
