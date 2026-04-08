bbbee6450762fd64db6878f6f24b4f5e75a0f48e
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-04-08 11:47:40 +0900

    [Platinum I] Title: 수열과 쿼리 6, Time: 2940 ms, Memory: 87352 KB -BaekjoonHub

--- 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.js
+++ 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.js
@@ -6,7 +6,7 @@ let [N, arr, M, ...queries] = inputValue
   .map((c) => c.trim().split(" ").map(Number));
 N = Number(N);
 M = Number(M);
-queries = queries.map((c, i) => [c[0], c[1], i]);
+queries = queries.map((c, i) => [c[0] - 1, c[1] - 1, i]);
 const DN = Math.sqrt(N);
 queries.sort((a, b) => {
   const [da, db] = [Math.floor(a[0] / DN), Math.floor(b[0] / DN)];
@@ -25,7 +25,7 @@ let prevLeft = initialPosition[0];
 let prevRight = initialPosition[1];
 let maxCnt = 0;
 for (let i = prevLeft; i <= prevRight; i++) {
-  const node = arr[i - 1];
+  const node = arr[i];
   dpForCnt[dp[node]]--;
   if (++dp[node] >= maxCnt) {
     maxCnt = dp[node];
@@ -36,46 +36,29 @@ answer[initialPosition[2]] = maxCnt;
 // initialize
 // console.log("init:", prevLeft, prevRight, maxCnt);
 
-function fncPlus(left, right) {
-  for (let i = left; i <= right; i++) {
-    const node = arr[i - 1];
-    dpForCnt[dp[node]]--;
-    if (++dp[node] > maxCnt) {
-      maxCnt = dp[node];
-    }
-    dpForCnt[dp[node]]++;
+function fncPlus(idx) {
+  const node = arr[idx];
+  dpForCnt[dp[node]]--;
+  if (++dp[node] > maxCnt) {
+    maxCnt = dp[node];
   }
+  dpForCnt[dp[node]]++;
 }
-function fncMinus(left, right) {
-  for (let i = left; i <= right; i++) {
-    const node = arr[i - 1];
+function fncMinus(idx) {
+  const node = arr[idx];
 
-    dpForCnt[dp[node]]--;
-    dp[node]--;
-    dpForCnt[dp[node]]++;
-    if (dpForCnt[maxCnt] === 0) maxCnt--;
-  }
+  dpForCnt[dp[node]]--;
+  dp[node]--;
+  dpForCnt[dp[node]]++;
+  if (dpForCnt[maxCnt] === 0) maxCnt--;
 }
 
 for (let i = 1; i < M; i++) {
   const [nodeLeft, nodeRight, nodeIdx] = queries[i];
-  if (nodeLeft < prevLeft) {
-    // 더하기
-    fncPlus(nodeLeft, prevLeft - 1);
-  } else if (nodeLeft > prevLeft) {
-    // 빼기
-    fncMinus(prevLeft, nodeLeft - 1);
-  }
-  if (nodeRight > prevRight) {
-    // 더하기
-    fncPlus(prevRight + 1, nodeRight);
-  } else if (nodeRight < prevRight) {
-    // 빼기
-    fncMinus(nodeRight + 1, prevRight);
-  }
-
-  prevLeft = nodeLeft;
-  prevRight = nodeRight;
+  while (nodeLeft < prevLeft) fncPlus(--prevLeft);
+  while (nodeLeft > prevLeft) fncMinus(prevLeft++);
+  while (nodeRight > prevRight) fncPlus(++prevRight);
+  while (nodeRight < prevRight) fncMinus(prevRight--);
   answer[nodeIdx] = maxCnt;
 }
 
