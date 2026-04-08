ba7aa9232d92800d6ca6251f02fe9e9155e6fb62
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-04-08 11:09:49 +0900

    [Platinum I] Title: 수열과 쿼리 6, Time: 2480 ms, Memory: 89244 KB -BaekjoonHub

--- 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.js
+++ 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.js
@@ -15,52 +15,44 @@ queries.sort((a, b) => {
 // queries sorting
 // console.log("queries:", queries);
 
-const answer = new Array(M).fill(0);
-const dp = new Array(100001).fill(0);
+const answer = new Uint32Array(M).fill(0);
+const dp = new Uint32Array(100001).fill(0); // 5가 현재 3개 존재 중
+const dpForCnt = new Uint32Array(100001).fill(0); // 7개인 값이 3개 존재 중
 
 const initialPosition = queries[0];
 let prevLeft = initialPosition[0];
 let prevRight = initialPosition[1];
 let maxCnt = 0;
-let maxNum = 0;
 for (let i = prevLeft; i <= prevRight; i++) {
   const node = arr[i - 1];
+  dpForCnt[dp[node]]--;
   if (++dp[node] >= maxCnt) {
-    maxNum = node;
     maxCnt = dp[node];
   }
+  dpForCnt[dp[node]]++;
 }
 answer[initialPosition[2]] = maxCnt;
 // initialize
-// console.log("init:", prevLeft, prevRight, maxCnt, maxNum);
+// console.log("init:", prevLeft, prevRight, maxCnt);
 
 function fncPlus(left, right) {
   for (let i = left; i <= right; i++) {
     const node = arr[i - 1];
+    dpForCnt[dp[node]]--;
     if (++dp[node] > maxCnt) {
       maxCnt = dp[node];
-      maxNum = node;
     }
+    dpForCnt[dp[node]]++;
   }
 }
 function fncMinus(left, right) {
-  let haveToReload = false;
   for (let i = left; i <= right; i++) {
     const node = arr[i - 1];
+
+    dpForCnt[dp[node]]--;
     dp[node]--;
-    if (node === maxNum) haveToReload = true;
-  }
-  return haveToReload;
-}
-function fncReload(left, right) {
-  maxNum = 0;
-  maxCnt = 0;
-  for (let i = left; i <= right; i++) {
-    const node = arr[i - 1];
-    if (dp[node] > maxCnt) {
-      maxCnt = dp[node];
-      maxNum = node;
-    }
+    dpForCnt[dp[node]]++;
+    if (dpForCnt[maxCnt] === 0) maxCnt--;
   }
 }
 
@@ -71,19 +63,19 @@ for (let i = 1; i < M; i++) {
     fncPlus(nodeLeft, prevLeft - 1);
   } else if (nodeLeft > prevLeft) {
     // 빼기
-    const haveToReload = fncMinus(prevLeft, nodeLeft - 1);
-    if (haveToReload) fncReload(nodeLeft, nodeRight);
+    fncMinus(prevLeft, nodeLeft - 1);
   }
   if (nodeRight > prevRight) {
     // 더하기
     fncPlus(prevRight + 1, nodeRight);
   } else if (nodeRight < prevRight) {
     // 빼기
-    const haveToReload = fncMinus(nodeRight + 1, prevRight);
-    if (haveToReload) fncReload(nodeLeft, nodeRight);
+    fncMinus(nodeRight + 1, prevRight);
   }
+
   prevLeft = nodeLeft;
   prevRight = nodeRight;
   answer[nodeIdx] = maxCnt;
 }
+
 console.log(answer.join("\n"));
