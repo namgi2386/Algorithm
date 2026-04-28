fa8231966716b63267eb84399dee7bdeb4d18e25
임남기 <143250820+namgi2386@users.noreply.github.com>
2026-04-28 09:45:45 +0900

    [level 1] Title: 노란불 신호등, Time: 26.39 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.js
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.js
@@ -1,30 +1,25 @@
 function solution(signals) {
-    // 345 , 10 11 12 = 3부터 5주기 = 3 + 5a (+2)
-    // 4 11 = 4부터 7주기 = 4 + 7b
-    // 3 7 11 = 3부터 4주기 = 3 + 4c
-    
-    // 2 8 14 2부터 +6 = 2 + 6a
-    // 3 9 15 3부터 +6b
-    // 4 10 16 4부터 +6c
-    // 5 11 17 5부터 +6d
-    const N = signals.length
-    const MAX = 10000000
-    const visited = new Array(MAX).fill(0)
-    let answer = -1
-    for(let i = 0; i<N;i++){
-        const [a,b,c] = signals[i]
-        const jump = a + b + c
-        
-        for(let v = a+1; v <MAX; v += jump ){
-            if(answer !== -1)break
-            for(let po = 0; po < b;po++){
-                visited[v + po]++
-                if(i === N-1 && visited[v + po] === N){
-                    answer = v + po
-                    break
-                }
-            }
-        }
+  function gcd(a, b) {
+    while (b !== 0) {
+      [a, b] = [b, a % b];
     }
-    return answer;
+    return a;
+  }
+
+  const MAX = signals.map(([a, b, c]) => a+b+c).reduce((a, b) => (a * b) / gcd(a, b));
+
+  for (let t = 1; t <= MAX; t++) {
+      let isPossible = true
+      for(const [a,b,c] of signals){
+          const sum = a+b+c
+          const po = (t-1) % sum
+          if(!(po >= a && po < a+b)){
+              isPossible = false
+              break
+          }
+      }
+      if(isPossible) return t
+  }
+
+  return -1;
 }
